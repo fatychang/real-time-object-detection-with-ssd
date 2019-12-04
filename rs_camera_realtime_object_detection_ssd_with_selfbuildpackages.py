7de6893b05ad2279ae5d17f5cf8bc6a7098812e7
JSChang <papachang12@gmail.com>
2019-12-04 14:58:56 +0800

    fix the bug that unable to rotate the pointcloud viewer by mouse. And add read from bag
    fix by passing the size of the out array to the class.
    Add the code to allow reading video from a .bag file

--- rs_camera_realtime_object_detection_ssd_with_selfbuildpackages.py
+++ rs_camera_realtime_object_detection_ssd_with_selfbuildpackages.py
@@ -72,6 +72,7 @@ COLORS = ssd.generate_colors(len(CLASSES))
 # config realsense camera pipeline
 pipeline = rs.pipeline()
 config = rs.config()
+rs.config.enable_device_from_file(config,"test.bag" )
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640 ,480, rs.format.bgr8, 30)
 
@@ -89,6 +90,7 @@ pc = rs.pointcloud()
 pc_state = AppState()
 # declare a numpy array to store the pointcloud data for visualization
 out = np.empty((480, 640, 3), dtype=np.uint8)
+pc_state.get_size(out)
 # declare colorizer instance
 colorizer = rs.colorizer()
 
